055a87b46b238b775b3c3b0ead1d6e6f6040e113
sandersirge <sandersirge@gmail.com>
2023-11-20 21:25:33 +0200

    muudatus

--- Avaleht/avaleht_efektid.js
+++ Avaleht/avaleht_efektid.js
@@ -1,6 +1,6 @@
-const mobileBtn = document.getElementById('mobile-cta');
-            nav = document.querySelector('nav');
-            mobileBtnExit = document.getElementById('mobile-exit');
+const mobileBtn = document.getElementById('mobiil-cta');
+    nav = document.querySelector('nav');
+    mobileBtnExit = document.getElementById('mobiil-välju');
 
 mobileBtn.addEventListener('click', () => {
     nav.classList.add('menu-btn');
