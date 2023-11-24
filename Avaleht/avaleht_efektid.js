f5a4eac74c2a33a01a2e44c664a57fb529f50c66
sandersirge <sandersirge@gmail.com>
2023-11-24 22:32:57 +0200

    Lisasin JS-ga intro avalehele

--- Avaleht/avaleht_efektid.js
+++ Avaleht/avaleht_efektid.js
@@ -9,3 +9,32 @@ mobileBtn.addEventListener('click', () => {
 mobileBtnExit.addEventListener('click', () => {
     nav.classList.remove('menu-btn');
 })
+
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.logo-päis');
+let logoSpan = document.querySelectorAll('.logo-intro');
+
+window.addEventListener('DOMContentLoaded', ()=>{
+    setTimeout(()=>{
+
+        logoSpan.forEach((span, idx)=>{
+            setTimeout(()=>{
+                span.classList.add('active');
+            }, (idx + 1) * 400)
+        });
+
+        setTimeout(()=>{
+            logoSpan.forEach((span, idx)=>{
+
+                setTimeout(()=>{
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, (idx + 1) * 50)
+            })
+        }, 2000);
+
+        setTimeout(()=>{
+            intro.style.top = '-100vh';
+        }, 2300)
+    })
+})
