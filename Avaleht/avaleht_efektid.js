6cb6723b2316389c66d62aaeea885e01eb48186b
sandersirge <sandersirge@gmail.com>
2023-11-26 00:33:39 +0200

    e-maili saatmine poolik, efektide täiustus

--- Avaleht/avaleht_efektid.js
+++ Avaleht/avaleht_efektid.js
@@ -38,3 +38,19 @@ window.addEventListener('DOMContentLoaded', ()=>{
         }, 2200)
     })
 })
+
+const form = document.querySelector('form');
+
+function sendEmail() {
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
