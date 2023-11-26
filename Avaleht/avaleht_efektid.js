63583ad5e0b144d9a4442ea54564696527ab8d69
sandersirge <sandersirge@gmail.com>
2023-11-26 15:54:09 +0200

    Kontaktivorm peaaegu töötav, väike error

--- Avaleht/avaleht_efektid.js
+++ Avaleht/avaleht_efektid.js
@@ -39,18 +39,97 @@ window.addEventListener('DOMContentLoaded', ()=>{
     })
 })
 
-const form = document.querySelector('form');
+const vorm = document.querySelector('form');
+const nimi =  document.getElementById('nimi');
+const email = document.getElementById('email');
+const telefon = document.getElementById('number');
+const pealkiri = document.getElementById('teema');
+const teade =  document.getElementById('sõnum');
 
 function sendEmail() {
+    const bodyMessage = `Nimi: ${nimi.value}<br> E-mail: ${email.value}<br>
+    Telefon: ${telefon.value}<br> Sõnum: ${teade.value}`;
+
     Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "username",
-        Password : "password",
-        To : 'them@website.com',
-        From : "you@isp.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
+        SecureToken: 'a270c788-ea75-48bf-b41c-08a6788cb77d',
+        To : 'sandersirge@gmail.com',
+        From : "projekt.kontaktivorm@gmail.com",
+        Subject : pealkiri.value,
+        Body : bodyMessage
     }).then(
-      message => alert(message)
+        message => {
+            if (message == 'OK') {
+                Swal.fire({
+                    title: "Saadetud!",
+                    text: "Teie sõnum on kätte saadud!",
+                    icon: "success"
+                });
+            }
+        }
     );
 }
+
+function checkInputs() {
+    const objektid = document.querySelectorAll('.objekt');
+
+    for (const objekt of objektid) {
+        if (objekt.value == '') {
+            objekt.classList.add('error');
+            objekt.parentElement.classList.add('error');
+        }
+
+        if (objektid[1].value != '') {
+            checkEmail();
+        }
+
+        objektid[1].addEventListener('keyup', () => {
+            checkEmail();
+        });
+
+        objekt.addEventListener('keyup', () => {
+            if (objekt.value != '') {
+                objekt.classList.remove('error');
+                objekt.parentElement.classList.remove('error');
+            }
+            else {
+                objekt.classList.add('error');
+                objekt.parentElement.classList.add('error');
+            }
+        });
+    }
+}
+
+function checkEmail() {
+    const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+    const errorTxtEmail = document.querySelector('.error-txt.email');
+
+    if (!email.value.match(emailRegex)) {
+        email.classList.add('error');
+        email.parentElement.classList.add('error');
+
+        if (email.value != '') {
+            errorTxtEmail.innertext = '*Sisesta sobiv e-posti aadress';
+        }
+        else {
+            errorTxtEmail.innertext = '*See on kohustuslik väli';
+        }
+    }
+    else {
+        email.classList.remove('error');
+        email.parentElement.classList.remove('error');
+    }
+}
+
+vorm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    checkInputs();
+
+    if (!nimi.classList.contains('error') && !email.classList.contains('error') 
+    && !telefon.classList.contains('error') && !pealkiri.classList.contains('error') 
+    && !email.classList.contains('error')) {
+        sendEmail();
+
+        vorm.reset();
+        return false;
+    }
+});
