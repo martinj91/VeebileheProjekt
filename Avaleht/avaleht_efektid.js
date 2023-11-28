b71d5576fb90de60010dfc8534dcd4c3768417fb
sandersirge <sandersirge@gmail.com>
2023-11-28 20:52:12 +0200

    Avalehe funktsionaalsuse parandamine

--- Avaleht/avaleht_efektid.js
+++ Avaleht/avaleht_efektid.js
@@ -14,23 +14,19 @@ var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
 var rootElement = document.documentElement;
 
 function handleScroll() {
-  // Do something on scroll
-  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
-  if (rootElement.scrollTop / scrollTotal > 0.8) {
-    // Show button
-    scrollToTopBtn.classList.add("showBtn");
-  } else {
-    // Hide button
-    scrollToTopBtn.classList.remove("showBtn");
-  }
+    var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+    if (rootElement.scrollTop / scrollTotal > 0.9) {
+        scrollToTopBtn.classList.add("showBtn");
+    } else {
+        scrollToTopBtn.classList.remove("showBtn");
+    }
 }
 
 function scrollToTop() {
-  // Scroll to top logic
-  rootElement.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
+    rootElement.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
 }
 scrollToTopBtn.addEventListener("click", scrollToTop);
 document.addEventListener("scroll", handleScroll);
