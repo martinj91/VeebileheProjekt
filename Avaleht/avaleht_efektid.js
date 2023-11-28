ebe8049174c5e05298ca5c0d9e7251d0abf15684
sandersirge <sandersirge@gmail.com>
2023-11-28 20:46:30 +0200

    Muudatused disainile

--- Avaleht/avaleht_efektid.js
+++ Avaleht/avaleht_efektid.js
@@ -10,6 +10,31 @@ mobileBtnExit.addEventListener('click', () => {
     nav.classList.remove('menu-btn');
 })
 
+var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+var rootElement = document.documentElement;
+
+function handleScroll() {
+  // Do something on scroll
+  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+  if (rootElement.scrollTop / scrollTotal > 0.8) {
+    // Show button
+    scrollToTopBtn.classList.add("showBtn");
+  } else {
+    // Hide button
+    scrollToTopBtn.classList.remove("showBtn");
+  }
+}
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
+document.addEventListener("scroll", handleScroll);
+
 let intro = document.querySelector('.intro');
 let logo = document.querySelector('.logo-päis');
 let logoSpan = document.querySelectorAll('.logo-intro');
